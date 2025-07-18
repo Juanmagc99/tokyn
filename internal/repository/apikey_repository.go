2e756c6ac2426ab3f935ec1b68142f676803db4d
Juanmagc99 <juanma101099@gmail.com>
2025-07-18 18:09:30 +0200

    repository

--- internal/repository/apikey_repository.go
+++ internal/repository/apikey_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"time"
 	"tokyn/internal/models"
 
 	"gorm.io/gorm"
@@ -19,9 +20,44 @@ func (repo *APIKeyRepository) Create(ak *models.APIKey) error {
 }
 
 func (repo *APIKeyRepository) Update(ak *models.APIKey) error {
+	if err := repo.db.Save(ak).Error; err != nil {
+		return err
+	}
 
+	return nil
 }
 
 func (repo *APIKeyRepository) FindByHash(hash string) (*models.APIKey, error) {
+	var ak models.APIKey
+	if err := repo.db.Where("key_hash = ? AND revoked = false", hash).First(&ak).Error; err != nil {
+		return nil, err
+	}
 
+	return &ak, nil
+}
+
+func (repo *APIKeyRepository) FindByID(id string) (*models.APIKey, error) {
+	var ak models.APIKey
+	if err := repo.db.Where("id = ? AND revoked = false", id).First(&ak).Error; err != nil {
+		return nil, err
+	}
+
+	return &ak, nil
+}
+
+func (repo *APIKeyRepository) Revoke(id string) error {
+	var ak models.APIKey
+	if err := repo.db.First(&ak, "id = ?", id).Error; err != nil {
+		return err
+	}
+
+	now := time.Now()
+	ak.Revoked = true
+	ak.RevokedAt = &now
+
+	if err := repo.db.Save(&ak).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
